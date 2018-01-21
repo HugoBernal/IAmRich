8141f7bb99c634c3353d4c43dfc0062a47f94023
Hugo Bernal <hugonobernal@gmail.com>
2018-01-21 12:52:37 -0500

    Initial push of files

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Hugo Bernal on Jan/21/18.
+//  Copyright © 2018 Hugo Bernal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
